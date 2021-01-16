d67681a356efb1a628c9dfcd7775151eed3972ce
pateljay15 <65332060+pateljay15@users.noreply.github.com>
2021-01-16 23:09:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const users = ['Jay', 'Kandarp', 'deven']
+
+const memberDiv = document.querySelector('.memberDiv')
+const addIcon = document.querySelector('.addIcon')
+
+const userIcons = () => {
+    users.reverse()
+    users.map((curElem) => {
+        memberDiv.insertAdjacentHTML('afterbegin', (
+            `<button class="btn"><span>${curElem}</span></button>`
+        ))
+    })
+}
+
+addIcon.addEventListener('click', () => {
+    let userName = prompt('Please enter your name')
+
+    if(userName !== null && !users.includes(userName)){
+        users.push(userName)
+        memberDiv.insertAdjacentHTML('afterbegin', (
+            `<button class="btn"><span>${userName}</span></button>`
+        ))
+        console.log(users)
+    } else{
+        alert('Username already exist!')
+    }
+})
+
+userIcons()
